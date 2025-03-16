abc995166784760777ee107405295b95c5f6d3c8
Kim <thkm.grd@gmail.com>
2025-03-16 23:07:18 +0100

    nsm

--- src/app/api/revalidate/route.ts
+++
@@ -1,7 +0,0 @@
-import { revalidatePath } from "next/cache"
-import { NextResponse } from "next/server";
-
-export const POST = async () => {
-	revalidatePath("/")
-	return NextResponse.json({ revalidated: true });
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
+"use client"
 
 import Links from "@/components/home/links";
 import { LinkType } from "@/db/schema";
-//import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { getLinks } from "@/actions/crud-links";
-import Revalidate from "@/components/home/revalidate";
 
-export default async function Home() {
-	// const [links, setLinks] = useState<LinkType[]>([])
-	// useEffect(() => {
-	// const getData = async () => {
-	// 	const data = await getLinks()
-	// 	setLinks(data)
-	// }
-	// getData()}, [])
-	const links = await getLinks()
+export default function Home() {
+	const [links, setLinks] = useState<LinkType[]>([])
+	useEffect(() => {
+	const getData = async () => {
+		const data = await getLinks()
+		setLinks(data)
+	}
+	getData()}, [])
 	return (
 		<div className="w-full min-h-screen flex flex-col justify-center items-center gap-20">	
-			<Revalidate />
 			<Links links={links} />
 		</div>
 	);
