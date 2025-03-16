3d03cf10b86eab9a8ddb31a1768c6aa1e2e62055
Kim <thkm.grd@gmail.com>
2025-03-16 22:56:43 +0100

    revalidate good for stats ptt

--- src/components/home/revalidate.tsx
+++ src/components/home/revalidate.tsx
@@ -1,13 +1,16 @@
 "use client"
 
 import { useEffect } from "react"
+import { useRouter } from "next/navigation"
 
 export default function Revalidate() {
+	const router = useRouter()
 	useEffect(() => {
 		const reva = async () => {
 			await fetch("/api/revalidate", {method: "POST"})
 		}
 		reva()
+		router.refresh()
 	}, [])
 	return null
 }
